c95067ccf3408999129fcd1f052855f8f028aecf
ullanor <pawel.nowacki95@wp.pl>
2021-08-22 14:01:26 +0200

    Commit for 22.08.2021

--- MiscClasses/NameToBrushConverterRates.cs
+++ MiscClasses/NameToBrushConverterRates.cs
@@ -12,10 +12,6 @@ namespace Przypominajka_3._0
     class NameToBrushConverterRates : IValueConverter
     {
         int input;
-        //char[] testArray;
-        //int r = 0;
-        //string number = string.Empty;
-        //float result = 0;
 
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
@@ -26,7 +22,7 @@ namespace Przypominajka_3._0
                 return Brushes.YellowGreen;
             if (input > 0)
                 return Brushes.Orange;
-            if (input < 0)
+            if (input < 0)      
                 return Brushes.Brown;
             return Brushes.Red;
         }
---
+++ MiscClasses/NameToNameConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Przypominajka_3._0
+{
+    class NameToNameConverter : IValueConverter
+    {
+        int input;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            input = int.Parse(value.ToString());
+            if (input > 1)
+                return value;
+            if (input > 0)
+                return "Tomorrow";
+            if (input > -1)
+                return "Today";
+            return "Expired";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
